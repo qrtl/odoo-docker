223e27c821156342e3665ef18a9602de99427267
kanda999 <kanda@quartile.co>
2022-09-05 12:40:10 +0800

    12.0 [IMP]Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# set the postgres database host, port, user and password according to the environment
+# and pass them as arguments to the odoo process if not present in the config file
+: ${HOST:=${DB_PORT_5432_TCP_ADDR:='172.17.0.1'}}
+: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoopwd'}}}
+
+DB_ARGS=()
+function check_config() {
+    param="$1"
+    value="$2"
+    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC"; then
+        conf_value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
+        if [ $conf_value != 'False' ]; then
+            value=$conf_value
+        fi
+    fi
+    DB_ARGS+=("--${param}")
+    DB_ARGS+=("${value}")
+}
+check_config "db_host" "$HOST"
+check_config "db_port" "$PORT"
+check_config "db_user" "$USER"
+check_config "db_password" "$PASSWORD"
+
+/usr/local/bin/wait-for-psql.py ${DB_ARGS[@]} --timeout=30
+python3 /odoo/code/odoo/odoo-bin -c $ODOO_RC "$@"
+
+exit 1
